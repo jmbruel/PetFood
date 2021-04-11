3024b303144fd53fd6a404d187af505d773f3fb3
Anton Brisilin <a.brisilin@innopolis.university>
2021-04-11 10:56:05 +0300

    Setup toolbar

--- app/src/main/java/com/mexator/petfoodinspector/ui/StartActivity.kt
+++ app/src/main/java/com/mexator/petfoodinspector/ui/StartActivity.kt
@@ -4,7 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.mexator.petfoodinspector.R
 import com.mexator.petfoodinspector.data.Repository
 import com.mexator.petfoodinspector.databinding.ActivityStartBinding
@@ -15,10 +19,22 @@ class StartActivity : AppCompatActivity() {
         ActivityStartBinding.inflate(layoutInflater)
     }
 
-    private val repo = Repository
+    private val navController by lazy {
+        Navigation.findNavController(binding.fragmentContainer)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setupActionBarWithNavController(navController, AppBarConfiguration(navController.graph))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController.popBackStack()
+        return super.onSupportNavigateUp()
     }
 }
