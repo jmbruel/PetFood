6b7ddcca8a7ac4b7330e0122a91ef8b89b51344e
Anton Brisilin <a.brisilin@innopolis.university>
2021-04-20 16:27:37 +0300

    Proper back arrow in toolbar in auth navigation flow

--- app/src/main/java/com/mexator/petfoodinspector/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/mexator/petfoodinspector/ui/auth/AuthActivity.kt
@@ -2,15 +2,31 @@ package com.mexator.petfoodinspector.ui.auth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.mexator.petfoodinspector.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityAuthBinding.inflate(layoutInflater)
     }
+    private val navController by lazy {
+        Navigation.findNavController(binding.fragmentContainer)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
